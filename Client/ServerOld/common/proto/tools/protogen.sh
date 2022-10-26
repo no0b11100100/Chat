a488f2d0d278c2eef86ac77a74e9eaa805c69d83
wlandos <fedorovskiy000@gmail.com>
2022-10-26 23:24:32 +0300

    Remove unused code

--- Client/ServerOld/common/proto/tools/protogen.sh
+++
@@ -1,2 +0,0 @@
-protoc --go_out=api --go_opt=paths=import --go-grpc_out=api --go-grpc_opt=paths=import --proto_path=. ./*.proto
-python3 -m grpc_tools.protoc -I . --python_out=api --grpc_python_out=api ./*.proto
